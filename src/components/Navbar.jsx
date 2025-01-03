adb650ef4138b6e9579acbd05257fdc8737d2a35
Saroj Kumar Dey <sarojkumardey.contact@gmail.com>
2025-01-03 18:06:21 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,19 @@
 import logo2 from "../assets/images/taxism.png";
 import logo from "../assets/images/taxismshort.png";
+import { motion } from "motion/react";
+import { AnimatePresence } from "motion/react";
+import { useState } from "react";
+import { IoMenu } from "react-icons/io5";
+import { RxCross2 } from "react-icons/rx";
+
+const slideVariants = {
+  hidden: { y: "10%", opacity: 0 },
+  visible: { y: "0%", opacity: 1 },
+  exit: { y: "-10%", opacity: 0 },
+};
+
 function Navbar() {
-  //use if need to change color on scroll
-
-  // const [isScrolled, setIsScrolled] = useState(false);
-
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const scrollTop = window.scrollY;
-  //     setIsScrolled(scrollTop > window.innerHeight * 0.8); // 80vh
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
+  const [sideBar, setSideBar] = useState(false);
 
   return (
     <div
@@ -40,16 +37,83 @@ function Navbar() {
       <div className="h-[3rem] aspect-square md:hidden">
         <img src={logo2} alt="logo2" className="w-full h-full object-cover" />
       </div>
-      <ul className="hidden h-full md:flex items-center gap-10 font-semibold text-gray-700  justify-betweenm text-lg">
-        <li className="text-[#015ea0]">Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Consultant</li>
-        <li>Career</li>
-        <li>Contact</li>
-      </ul>
+
+      <div className="md:hidden">
+        <AnimatePresence mode="wait">
+          {sideBar ? (
+            <motion.div
+              key="close-icon"
+              variants={slideVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            >
+              <RxCross2
+                style={{ fontSize: "2.5rem" }}
+                onClick={() => setSideBar(false)}
+                className="text-[#012b4a]"
+              />
+            </motion.div>
+          ) : (
+            <motion.div
+              key="menu-icon"
+              variants={slideVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            >
+              <IoMenu
+                style={{ fontSize: "2.5rem" }}
+                onClick={() => setSideBar(true)}
+                className="text-[#012b4a]"
+              />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+      <div className="hidden md:flex">
+        <NavLinks />
+      </div>
     </div>
   );
 }
 
 export default Navbar;
+
+export const NavLinks = () => {
+  const isActive = (path) => location.pathname === path;
+
+  const navItems = [
+    { path: "/", label: "Home" },
+    { path: "/about", label: "About" },
+    { path: "/consultant", label: "Consultant" },
+    { path: "/career", label: "Career" },
+    { path: "/contact", label: "Contact" },
+  ];
+  return (
+    <ul className="flex h-full flex-col  md:flex-row items-center gap-10 font-semibold text-gray-700  justify-center text-lg ">
+      {navItems.map(({ path, label }) => (
+        <NavItem
+          key={path}
+          path={path}
+          label={label}
+          isActive={isActive(path)}
+        />
+      ))}
+    </ul>
+  );
+};
+
+const NavItem = ({ path, label, isActive }) => {
+  return (
+    <li
+      className={`${
+        isActive ? "text-[#015ea0]" : "text-gray-700"
+      } cursor-pointer hover:scale-105 transition-all hover:-translate-y-[1px]  select-none`}
+    >
+      {label}
+    </li>
+  );
+};
