83917d2fb709c7086dc11f0080acf26d1ec37b4b
Saroj Kumar Dey <sarojkumardey.contact@gmail.com>
2025-01-03 21:46:35 +0530

    changes

--- src/components/Home/Slider.jsx
+++ src/components/Home/Slider.jsx
@@ -10,7 +10,7 @@ function Slider() {
       />
       <div className="absolute inset-0 w-full h-full bg-gradient-to-b from-[#04090cba] via-[#04090cba] to-[#003d554a]  "></div>
       <div className="absolute inset-0 w-full h-full z-10 flex flex-col justify-center items-center text-center">
-        <h2 className="text-3xl md:text-5xl leading-[1.2]  font-medium w-[90%] md:w-[70%] lg:w-[50%]  text-gray-200 font-montserrat mt-20">
+        <h2 className="text-3xl md:text-5xl leading-[1.2]  font-medium w-[90%] md:w-[70%] lg:w-[50%]  text-gray-200 font-montserrat  mt-8 md:mt-20">
           Precision Planning for a Profitable Future.
         </h2>
         <button className="bg-gradient-to-b from-blue-500 to-blue-700 text-xl text-gray-100 px-5 py-3 rounded-full font-semibold mt-14">
